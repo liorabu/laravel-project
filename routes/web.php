1669c8be1b4514ecda58df51e555f33678cd96eb
moria zaken <moriaznoy@gmail.com>
2019-08-14 14:12:39 +0300

    dash2

--- routes/web.php
+++ routes/web.php
@@ -31,21 +31,10 @@ Route::group(['middleware' => 'auth'], function () {
 		return view('pages.icons');
 	})->name('icons');
 
-	Route::get('map', function () {
-		return view('pages.map');
-	})->name('map');
-
 	Route::get('notifications', function () {
 		return view('pages.notifications');
 	})->name('notifications');
 
-	Route::get('rtl-support', function () {
-		return view('pages.language');
-	})->name('language');
-
-	Route::get('upgrade', function () {
-		return view('pages.upgrade');
-	})->name('upgrade');
 });
 
 Route::group(['middleware' => 'auth'], function () {
